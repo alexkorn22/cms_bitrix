66cadbef3fc12df0db7f01e1727f6c929514a19b
AhmedMarzouk266 <AhmedMarzouk266@gmail.com>
2018-03-12 16:23:25 +0200

    Исправить название шаблона

--- www/local/templates/wedo/components/bitrix/news/catalog/section.php
+++ www/local/templates/wedo/components/bitrix/news/catalog/section.php
@@ -15,7 +15,7 @@ $this->setFrameMode(true);
 
 <?$APPLICATION->IncludeComponent(
 	"bitrix:news.list",
-	"default",
+	"",
 	Array(
 		"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
 		"IBLOCK_ID" => $arParams["IBLOCK_ID"],
