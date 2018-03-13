347a5a105656819855fe3d247035d118888b00d6
logvinova <julia.logvinova@freshcode.org>
2018-03-13 16:54:49 +0200

    начал секцию wmf cms

--- www/local/templates/wedo/js/src/main.js
+++ www/local/templates/wedo/js/src/main.js
@@ -1,10 +1,11 @@
 $( document ).ready(function() {
 
-
+/*burger menu*/
     $('.burger').on('click',function() {
         $('.top-fixed-line').toggleClass('open');
         $('.burger').toggleClass('open');
         $('.overlay').toggleClass('open');
     });
+/*END burger menu*/
 
 });
