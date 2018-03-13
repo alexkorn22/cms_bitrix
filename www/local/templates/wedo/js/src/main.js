94ddd5cda4efa70b2e41230a9e527fe40046a793
logvinova <julia.logvinova@freshcode.org>
2018-03-13 22:33:36 +0200

    плавающее меню доработки блока wmf-cms

--- www/local/templates/wedo/js/src/main.js
+++ www/local/templates/wedo/js/src/main.js
@@ -20,6 +20,46 @@ $( document ).ready(function() {
         autoplaySpeed: 2000
 });
 
+    $( ".block.wmf" ).hover(function() {
+        $( '.block.cms .block-title' ).css('opacity','0');
+     }, function(){
+            $( '.block.cms .block-title' ).css('opacity','1');
+    });
 
+    $( ".block.cms" ).hover(function() {
+        $( '.block.wmf .block-title' ).css('opacity','0');
+    },  function(){
+        $( '.block.wmf .block-title' ).css('opacity','1');
+    });
 
+    $('.burger').on('click', function () {
+        $('#line-menu').toggleClass("none")
+    })
 });
+
+
+// SCROLL NAVBAR
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("line-menu");
+var sticky = navbar.offsetTop;
+var prevScrollpos = window.pageYOffset;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        navbar.style.transition = "all 0.5s";
+        navbar.style.top = "0";
+    } else {
+        navbar.style.transition = "all 0.5s";
+        navbar.style.top = "-115px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+// END SCROLL
