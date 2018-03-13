b97e34273d221be85d2ed2899065801a55bf6724
AhmedMarzouk266 <AhmedMarzouk266@gmail.com>
2018-03-13 14:17:04 +0200

    Добавить класс Currency

---
+++ www/local/classes/Currency.php
@@ -0,0 +1,53 @@
+<?php
+
+use Bitrix\Highloadblock\HighloadBlockTable;
+
+class Currency
+{
+    private $currency;
+    private $id;
+    private $rate ;
+    private $multiplicity ;
+
+    const EUR = 5;
+    const USD = 6;
+    const UAH = 7;
+
+    public function __construct($idCurrency)
+    {
+        $this->id   = $idCurrency;
+        $this->setCurrencyData();
+    }
+
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    public function getRate(){
+        return $this->rate;
+    }
+
+
+    public static function convert($amount= 1,$from,$to){
+       $ConvertedPrice = $amount * ( ($from->rate/$from->multiplicity) / ($to->rate/$to->multiplicity) );
+       return $ConvertedPrice;
+    }
+
+    public function setCurrencyData(){
+        $arHLBlock          = HighloadBlockTable::getById(1)->fetch();
+        $obEntity           = HighloadBlockTable::compileEntity($arHLBlock);
+        // git list :
+        $strEntityDataClass = $obEntity->getDataClass();
+        $rsData             = $strEntityDataClass::getList();
+        while ($arItem = $rsData->Fetch()) {
+            if($arItem['ID'] == $this->id){
+                $this->rate         = $arItem['UF_RATE'];
+                $this->multiplicity = $arItem['UF_MULTIPLICITY'];
+            }
+        }
+    }
+
+}
+
+
