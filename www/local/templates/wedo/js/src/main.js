66d122715d1e4f7dad69ff7d24ea3bdf0ec2d8e5
logvinova <julia.logvinova@freshcode.org>
2018-03-14 13:00:32 +0200

    допиливаю секцию wmf-cms допиливаю секцию клиенты (слайдер) запилил футер

--- www/local/templates/wedo/js/src/main.js
+++ www/local/templates/wedo/js/src/main.js
@@ -8,35 +8,62 @@ $( document ).ready(function() {
     });
 /*END burger menu*/
 
-
+/*slider clients*/
     $('.center').slick({
         centerMode: true,
         infinite: true,
-        slidesToShow: 3,
+        slidesToShow: 5,
         focusOnSelect: true,
         centerPadding: '60px',
         speed: 500,
         autoplay: true,
-        autoplaySpeed: 2000
+        autoplaySpeed: 2000,
+        arrows: true,
+        pauseOnFocus: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
 });
+/*END slider clients*/
 
+/*section wmf-cms*/
     $( ".block.wmf" ).hover(function() {
-        $( '.block.cms .block-title' ).css('opacity','0');
+        $( '.block.cms .block-title' ).css({'opacity':'0','transition':'0.1s'});
      }, function(){
-            $( '.block.cms .block-title' ).css('opacity','1');
+            $( '.block.cms .block-title' ).css({'opacity':'1', 'transition':'0.5s', 'transition-delay':'0.8s'});
     });
 
     $( ".block.cms" ).hover(function() {
-        $( '.block.wmf .block-title' ).css('opacity','0');
+        $( '.block.wmf .block-title' ).css({'opacity':'0', 'transition':'0.1s'});
     },  function(){
-        $( '.block.wmf .block-title' ).css('opacity','1');
+        $( '.block.wmf .block-title' ).css({'opacity':'1', 'transition':'0.5s', 'transition-delay':'0.8s'});
     });
 
     $('.burger').on('click', function () {
         $('#line-menu').toggleClass("none")
     })
 });
-
+/*END section wmf-cms*/
 
 // SCROLL NAVBAR
 window.onscroll = function() {myFunction()};
