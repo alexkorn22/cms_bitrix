88591c9af3ea40d5bd3b1bfc0235edebc34ae18d
Александр Корниенко <kornsasha89@gmail.com>
2018-03-14 13:14:18 +0200

    Добавлено главное меню

--- www/index.php
+++ www/index.php
@@ -2,7 +2,6 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Кофемашины");
 ?>
-
-    <h1><a href="catalog">Catalog Coffee-Machines</a></h1>
+Главная
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- www/local/templates/wedo/header.php
+++ www/local/templates/wedo/header.php
@@ -20,6 +20,26 @@
 </head>
 <body>
 <?$APPLICATION->ShowPanel();?>
-
+<header>
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:menu",
+        "blue_tabs",
+        array(
+            "ALLOW_MULTI_SELECT" => "N",
+            "CHILD_MENU_TYPE" => "top",
+            "DELAY" => "N",
+            "MAX_LEVEL" => "1",
+            "MENU_CACHE_GET_VARS" => array(
+            ),
+            "MENU_CACHE_TIME" => "3600",
+            "MENU_CACHE_TYPE" => "Y",
+            "MENU_CACHE_USE_GROUPS" => "Y",
+            "ROOT_MENU_TYPE" => "top",
+            "USE_EXT" => "N",
+            "COMPONENT_TEMPLATE" => "blue_tabs"
+        ),
+        false
+    );?>
+</header>
 
 	
