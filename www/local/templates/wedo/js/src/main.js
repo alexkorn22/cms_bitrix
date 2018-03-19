7ee2bf3e1f5a39759755a085c4e398c71725b25f
sergeydem <s.demjanenko@artorg.com.ua>
2018-03-19 11:36:14 +0200

    видео на моб убрал. вместо картинка адаптивность слайдера адаптивность под моб и планшеты

--- www/local/templates/wedo/js/src/main.js
+++ www/local/templates/wedo/js/src/main.js
@@ -28,13 +28,19 @@ $( document ).ready(function() {
                 }
             },
             {
-                breakpoint: 600,
+                breakpoint: 992,
                 settings: {
                     slidesToShow: 3
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
                 settings: {
                     slidesToShow: 1,
                     centerMode: false
