6e12fbd0e8751356e3f6d6522995614f1a0e4138
AhmedMarzouk266 <AhmedMarzouk266@gmail.com>
2018-03-20 15:06:13 +0200

    Применять верстку раздела каталога

--- www/index.php
+++ www/index.php
@@ -1,7 +1,224 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Кофемашины");
-?>Главная <br>
+?>
+
+    <section id="cards-section">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-lg-10">
+                    <div class="section-title">
+                        <h2 class="title">Профессиональные кофемашины WMF от официального дилера Coffee Machine Service</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="menu-cards">
+                <div class="row justify-content-center">
+                    <div class="col-md-3 dont-padding">
+                        <div class="m-card first">
+                            <div class="wrap">
+                                <img src="<?=SITE_TEMPLATE_PATH?>/images/icon-sale.png" alt="sale">
+                                <div>
+                                    <span>Продажа</span>
+                                    <div class="meta">
+                                        <ul>
+                                            <li>для офисов компаний, предприятий</li>
+                                            <li>гостиниц и отелей со шведскими линиями</li>
+                                            <li>фастфудов</li>
+                                            <li>АЗС</li>
+                                        </ul>
+                                        <a href="#" class="more-info">Подробнее</a>
+                                        <div class="wrap-arrow d-none d-md-flex">
+                                            <div class="scroll-arrow"></div>
+                                            <div class="scroll-arrow"></div>
+                                            <div class="scroll-arrow"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 dont-padding">
+                        <div class="m-card second">
+                            <div class="wrap">
+                                <img src="<?=SITE_TEMPLATE_PATH?>/images/icon-lease.png" alt="lease">
+                                <div>
+                                    <span>Аренда</span>
+                                    <div class="meta">
+                                        <ul>
+                                            <li>без стартового взноса</li>
+                                            <li>без ежемесячных затрат</li>
+                                            <li>без процента от продаж</li>
+                                            <li>без залоговой суммы</li>
+                                        </ul>
+                                        <a href="#" class="more-info">Подробнее</a>
+                                        <div class="wrap-arrow d-none d-md-flex">
+                                            <div class="scroll-arrow"></div>
+                                            <div class="scroll-arrow"></div>
+                                            <div class="scroll-arrow"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 dont-padding">
+                        <div class="m-card third">
+                            <div class="wrap">
+                                <img src="<?=SITE_TEMPLATE_PATH?>/images/icon-repairs.png" alt="repairs">
+                                <div>
+                                    <span>Ремонт</span>
+                                    <div class="meta">
+                                        <ul>
+                                            <li>выезд техника на место</li>
+                                            <li>диагностика неисправности</li>
+                                            <li>подбор и замена деталей и узлов</li>
+                                            <li>капитальный ремонт по истечении гарантии</li>
+                                        </ul>
+                                        <a href="#" class="more-info">Подробнее</a>
+                                        <div class="wrap-arrow d-none d-md-flex">
+                                            <div class="scroll-arrow"></div>
+                                            <div class="scroll-arrow"></div>
+                                            <div class="scroll-arrow"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 dont-padding">
+                        <div class="m-card fourth">
+                            <div class="wrap">
+                                <img src="<?=SITE_TEMPLATE_PATH?>/images/icon-service.png" alt="service">
+                                <div>
+                                    <span>Сервис</span>
+                                    <div class="meta">
+                                        <ul>
+                                            <li>ежемесячный визит техника</li>
+                                            <li>проверка технических параметров</li>
+                                            <li>настройка датчиков</li>
+                                            <li>текущий ремонт</li>
+                                        </ul>
+                                        <a href="#" class="more-info">Подробнее</a>
+                                        <div class="wrap-arrow d-none d-md-flex">
+                                            <div class="scroll-arrow"></div>
+                                            <div class="scroll-arrow"></div>
+                                            <div class="scroll-arrow"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="wmf-cms">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-lg-10">
+                    <div class="section-title">
+                        <h2 class="title">Профессиональные кофемашины для бизнеса, соответствующие любым требованиям</h2>
+                    </div>
+                    <div class="section-description">
+                        <p class="descr">Coffee Machine Service позволяет приобрести профессиональные кофемашины WMF (Германия) в Украине с полным
+                            сервисным сопровождением. Предлагая вам суперавтоматы WMF, мы ориентируемся на потребности
+                            вашего бизнеса и решение ваших задач</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="wrap-wmf-cms">
+                    <div class="block wmf">
+                        <div class="block-title">
+                            <p><span class="firm">WMF - </span><span>самые мощные профессиональные</span><span> кофемашины-суперавтоматы</span></p>
+                        </div>
+                        <div class="content-wmf-cmf">
+                            <div class="c-image">
+                                <img src="<?=SITE_TEMPLATE_PATH?>/images/wmf-image.png" class="d-none d-lg-block" alt="WMF">
+                            </div>
+                            <div class="c-text">
+                                <ul>
+                                    <li>для работы в местах с большим потоком людей</li>
+                                    <li>не требуют работы баристa</li>
+                                    <li>гарантийный срок 30000 порций</li>
+                                    <li>сервисное обслуживание и ремонт в Украине</li>
+                                </ul>
+                                <div class="wrap-link">
+                                    <div class="wrap-arrow">
+                                        <div class="scroll-arrow"></div>
+                                        <div class="scroll-arrow"></div>
+                                        <div class="scroll-arrow"></div>
+                                    </div>
+                                    <a href="#" class="more-info">Каталог кофемашин</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="block cms">
+                        <div class="block-title">
+                            <p><span class="firm">Coffee Machine Service</span><span> официальный партнёр </span><span>компании WMF в Украине</span></p>
+                        </div>
+                        <div class="content-wmf-cmf">
+                            <div class="c-image">
+                                <img src="<?=SITE_TEMPLATE_PATH?>/images/cms-image.png" class="d-none d-lg-block" alt="CMS">
+                            </div>
+                            <div class="c-text">
+                                <ul>
+                                    <li>Подбор кофеавтомата под ваши задачи</li>
+                                    <li>Бесплатный сервис в любом городе Украины</li>
+                                    <li>Пять центров по ремонту</li>
+                                    <li>Оригинальные запчасти на профессиональные кофемашины WMF</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="clients">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-lg-10">
+                    <div class="section-title">
+                        <h2 class="title">Наши клиенты</h2>
+                    </div>
+                    <div class="section-description">
+                        <p class="descr">Профессиональные кофемашины WMF приносят прибыль владельцам супермаркетов, фастфудов,
+                            кофеен, АЗС, отелей и компаний по всей Украине благодаря сотрудничеству с компанией Coffee Machine Service.
+                            Станьте одним из них!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="wrap-slider">
+                <div class="center">
+                    <div class="slide">
+                        <img src="<?=SITE_TEMPLATE_PATH?>/images/client1.png" alt="">
+                    </div>
+                    <div class="slide">
+                        <img src="<?=SITE_TEMPLATE_PATH?>/images/client2.png" alt="">
+                    </div>
+                    <div class="slide">
+                        <img src="<?=SITE_TEMPLATE_PATH?>/images/client5.png" alt="">
+                    </div>
+                    <div class="slide">
+                        <img src="<?=SITE_TEMPLATE_PATH?>/images/client3.png" alt="">
+                    </div>
+                    <div class="slide">
+                        <img src="<?=SITE_TEMPLATE_PATH?>/images/client1.png" alt="">
+                    </div>
+                    <div class="slide">
+                        <img src="<?=SITE_TEMPLATE_PATH?>/images/client4.png" alt="">
+                    </div>
+                    <div class="slide">
+                        <img src="<?=SITE_TEMPLATE_PATH?>/images/client5.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
-    <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ www/local/files/includes/contacts.php
@@ -0,0 +1,13 @@
+<div class="f-contact">
+    <div class="wrap-social-icons footer">
+        <div class="social-icons footer">
+            <a class="phone d-none d-sm-block" href="tel:+38 (050) 449-06-00">+38 (050) 449-06-00</a>
+            <a class="email d-none d-sm-block" href="mailto:cms@cms.com.ua">cms@cms.com.ua</a>
+            <div class="icon">
+                <a href="#" class="social facebook"></a>
+                <a href="#" class="social insta"></a>
+                <a href="#" class="social youtube"></a>
+            </div>
+        </div>
+    </div>
+</div>
