f34bfdae83357451496d0b78a4b9682336e9cecb
sergeydem <s.demjanenko@artorg.com.ua>
2018-03-22 16:26:40 +0200

    слайдер. лучше но еще кривой характеристики. цена. кнопка описание + на js кнопка подробнее

--- www/local/templates/wedo/js/src/main.js
+++ www/local/templates/wedo/js/src/main.js
@@ -98,6 +98,7 @@ $( document ).ready(function() {
         slidesToScroll: 1,
         arrows: false,
         fade: true,
+        adaptiveHeight: true,
         asNavFor: '.product-img-preview'
     });
     $('.product-img-preview').slick({
@@ -105,10 +106,54 @@ $( document ).ready(function() {
         slidesToScroll: 1,
         asNavFor: '.product-img-main',
         dots: false,
-        centerMode: true,
+        nav: false,
+        vertical: true,
+        arrows:false,
         focusOnSelect: true
     });
 //Конец слайдер в карточке товара
+
+
+//карточка товара - описание - показать больше
+
+    // Show more text option
+    var showChar = 500;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Подробнее";
+    var lesstext = "Скрыть";
+
+    //Cut content based on showChar length
+    if ($(".toggle-text").length) {
+        $(".toggle-text").each(function() {
+
+            var content = $(this).html();
+
+            if(content.length > showChar) {
+
+                var contentExcert = content.substr(0, showChar);
+                var contentRest = content.substr(showChar, content.length - showChar);
+                var html = contentExcert + '<span class="toggle-text-ellipses">' + ellipsestext + ' </span> <span class="toggle-text-content"><span>' + contentRest + '</span><a href="javascript:;" class="toggle-text-link">' + moretext + '</a></span>';
+
+                $(this).html(html);
+            }
+        });
+    }
+
+    //Toggle content when click on read more link
+    $(".toggle-text-link").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+
+//конец карточка товара - описание - показать больше
 
 });
 
