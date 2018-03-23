730b9e95bf0d166dfa8342f002af13832a8ab0cb
sergeydem <s.demjanenko@artorg.com.ua>
2018-03-23 16:28:22 +0200

    валидация формы "заказать консультацию"

--- www/local/templates/wedo/js/src/main.js
+++ www/local/templates/wedo/js/src/main.js
@@ -169,6 +169,28 @@ $( document ).ready(function() {
     });
 //КОнец кнопка на верх
 
+
+//валидация форм
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('consultation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
+//Конец валидация форм
+
 });
 
 
