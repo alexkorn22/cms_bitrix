88e587fd66691da45f5fe4050ff46ef411d38196
Ahmed Marzouk <AhmedMarzouk266@gmail.com>
2018-03-26 16:58:13 +0300

    добавить включение области

---
+++ www/local/files/includes/cards/remont.php
@@ -0,0 +1,22 @@
+<div class="m-card third">
+    <div class="wrap">
+        <img src="<?=IMAGES_PATH?>/icon-repairs.png" alt="repairs">
+        <div>
+            <span>Ремонт</span>
+            <div class="meta">
+                <ul>
+                    <li>выезд техника на место</li>
+                    <li>диагностика неисправности</li>
+                    <li>подбор и замена деталей и узлов</li>
+                    <li>капитальный ремонт по истечении гарантии</li>
+                </ul>
+                <a href="#" class="more-info">Подробнее</a>
+                <div class="wrap-arrow d-none d-md-flex">
+                    <div class="scroll-arrow"></div>
+                    <div class="scroll-arrow"></div>
+                    <div class="scroll-arrow"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
