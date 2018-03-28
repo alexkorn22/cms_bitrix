71973789ab3361ff2e8a3690d2777a466ca5c6ca
Ahmed Marzouk <AhmedMarzouk266@gmail.com>
2018-03-28 16:37:45 +0300

    исправить alt для фотографии похожые товары

--- www/local/components/wedo/similar_products/templates/.default/template.php
+++ www/local/components/wedo/similar_products/templates/.default/template.php
@@ -2,7 +2,7 @@
     <?foreach($arResult['ITEMS'] as $item):?>
         <div class="similar-product">
             <a href="<?=$item['DETAIL_PAGE_URL']?>/">
-                <img src="<?=CFile::GetPath($item['PREVIEW_PICTURE'])?>" alt="">
+                <img src="<?=CFile::GetPath($item['PREVIEW_PICTURE'])?>" alt="<?=$item['NAME']?>">
                 <p><?=$item['NAME']?></p>
             </a>
         </div>
