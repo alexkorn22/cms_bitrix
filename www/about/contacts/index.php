8283d2f8623b472bbc4d3ab0b4c2616c33e4e098
Ahmed Marzouk <AhmedMarzouk266@gmail.com>
2018-04-10 13:11:11 +0300

    Неправильные хлебные крошки в Контактах

--- www/about/contacts/index.php
+++ www/about/contacts/index.php
@@ -1,5 +1,6 @@
 <?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("");
+$APPLICATION->AddChainItem("Контакты", "/about/contacts");
 ?><div class="container-fluid">
 	<div class="row">
 		<div class="col-md-7">
