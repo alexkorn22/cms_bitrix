adf7195864362ec9d9ba12665a8c61afd6be34f8
Ahmed Marzouk <AhmedMarzouk266@gmail.com>
2018-04-16 17:03:14 +0300

    М-98. В меню: при клике на страницу, на которой ты сейчас находишься и с которой вызывал меню, меню должно сворачиваться.

--- www/local/templates/wedo/components/bitrix/menu/left/template.php
+++ www/local/templates/wedo/components/bitrix/menu/left/template.php
@@ -1,10 +1,15 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?if (!empty($arResult)):?>
+<? $curPage = $APPLICATION->GetCurPage(); ?>
 
     <div class="f-menu left">
         <ul>
             <? foreach($arResult as $arItem):?>
-            <li class="item"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+                <? if($curPage == $arItem["LINK"]) :?>
+                    <li class="item"><a class="burger" href="#"><?=$arItem["TEXT"]?></a></li>
+                <? else:?>
+                    <li class="item"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+                <?endif;?>
             <?endforeach?>
         </ul>
     </div>
--- www/local/templates/wedo/components/bitrix/menu/top/template.php
+++ www/local/templates/wedo/components/bitrix/menu/top/template.php
@@ -1,10 +1,14 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?if (!empty($arResult)):?>
-
+<? $curPage = $APPLICATION->GetCurPage(); ?>
     <div class="f-menu right">
         <ul>
             <? foreach($arResult as $arItem):?>
-                <li class="item"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+                <? if($curPage == $arItem["LINK"]) :?>
+                    <li class="item"><a class="burger" href="#"><?=$arItem["TEXT"]?></a></li>
+                <? else:?>
+                    <li class="item"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+                <?endif;?>
             <?endforeach?>
         </ul>
     </div>
