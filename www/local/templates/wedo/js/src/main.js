82afebda32ca503452311997b54d1e285a1e0793
sergeydem <s.demjanenko@artorg.com.ua>
2018-04-19 09:31:36 +0300

    убрал консоль лог

--- www/local/templates/wedo/js/src/main.js
+++ www/local/templates/wedo/js/src/main.js
@@ -563,7 +563,6 @@ function TagsBlog() {
         availableSpace = $vlinks.width() - 10;
         numOfVisibleItems = $vlinks.children().length;
         requiredSpace = breakWidths[numOfVisibleItems - 1];
-        console.log(requiredSpace)
         // Not enough space
         if (requiredSpace > availableSpace) {
             $vlinks.children().last().prependTo($hlinks);
