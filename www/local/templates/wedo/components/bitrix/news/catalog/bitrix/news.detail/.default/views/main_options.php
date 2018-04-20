c45185fd627a514b99f17a764908f1c13c5948f9
Ahmed Marzouk <AhmedMarzouk266@gmail.com>
2018-04-20 16:08:39 +0300

    сделать разметку 4

--- www/local/templates/wedo/components/bitrix/news/catalog/bitrix/news.detail/.default/views/main_options.php
+++ www/local/templates/wedo/components/bitrix/news/catalog/bitrix/news.detail/.default/views/main_options.php
@@ -1,26 +1,26 @@
-<div itemscope itemtype="http://schema.org/Product">
-    <p itemprop="name" class="d-none"><?=$arResult['TITLE_H1']?></p>
-    <div id="osnovnoe">
+<div id="osnovnoe">
     <div class="row">
         <div class="col-lg-6">
             <div class="image-product">
                 <div class="row">
                     <div class="col-md-9 col-lg-9">
                         <div class="product-img-main">
-                            <? foreach($arResult['MAIN_IMAGES'] as $img):?>
+                            <? foreach ($arResult['MAIN_IMAGES'] as $img): ?>
                                 <div class="img-main">
-                                    <img src="<?=$img['SRC']?>" class="img-fluid" alt="<?=$img['ALT']?>" title="<?=$img['TITLE']?>">
+                                    <img src="<?= $img['SRC'] ?>" class="img-fluid" alt="<?= $img['ALT'] ?>"
+                                         title="<?= $img['TITLE'] ?>">
                                 </div>
-                            <? endforeach;?>
+                            <? endforeach; ?>
                         </div>
                     </div>
                     <div class="col-md-3 col-lg-2">
                         <div class="product-img-preview">
-                            <? foreach($arResult['MAIN_IMAGES'] as $img):?>
+                            <? foreach ($arResult['MAIN_IMAGES'] as $img): ?>
                                 <div class="preview">
-                                    <img src="<?=$img['SRC']?>" class="img-fluid" alt="<?=$img['ALT']?>" title="<?=$img['TITLE']?>"  itemprop="image">
+                                    <img src="<?= $img['SRC'] ?>" class="img-fluid" alt="<?= $img['ALT'] ?>"
+                                         title="<?= $img['TITLE'] ?>" itemprop="image">
                                 </div>
-                            <? endforeach;?>
+                            <? endforeach; ?>
                         </div>
                     </div>
                 </div>
@@ -32,23 +32,26 @@
                     <p class="title-charact">Характеристики</p>
                     <div class="characteristics">
                         <div class="list-group">
-                            <?foreach($arResult['PROPERTIES'] as $prop):?>
-                                <?if(!empty($prop['VALUE']) && $prop['SORT'] < 5000 ):?>
-                                    <p class="item"><span><?=$prop['NAME']?></span><span><?=$prop['VALUE']?></span></p>
-                                <?endif;?>
-                            <?endforeach;?>
+                            <? foreach ($arResult['PROPERTIES'] as $prop): ?>
+                                <? if (!empty($prop['VALUE']) && $prop['SORT'] < 5000): ?>
+                                    <p class="item"><span><?= $prop['NAME'] ?></span><span><?= $prop['VALUE'] ?></span>
+                                    </p>
+                                <? endif; ?>
+                            <? endforeach; ?>
                         </div>
                     </div>
                 </div>
                 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-                     <div class="product-price">
-                         <p><?=$arResult['PRICE_UAH']?> грн.<span>*</span></p>
-                         <span itemprop="price" class="d-none"><?=$arResult['PRICE_UAH']?></span>
-                         <span itemprop="priceCurrency" class="d-none">UAH</span>
+                    <div class="product-price">
+                        <p><?= $arResult['PRICE_UAH'] ?> грн.<span>*</span></p>
+                        <span itemprop="price" class="d-none"><?= $arResult['PRICE_UAH'] ?></span>
+                        <span itemprop="priceCurrency" class="d-none">UAH</span>
                     </div>
                 </div>
                 <div class="product-button">
-                    <button type="button" class="btn btn_red btn_small-mobile" data-toggle="modal" data-target="#order-consultation">Заказать консультацию</button>
+                    <button type="button" class="btn btn_red btn_small-mobile" data-toggle="modal"
+                            data-target="#order-consultation">Заказать консультацию
+                    </button>
                 </div>
                 <div class="price-comment">
                     <p><span>*</span> Цена за базовую модель 1</p>
@@ -61,10 +64,9 @@
         <div class="row">
             <div class="col">
                 <p class="title">Описание</p>
-                <p class="text toggle-text" itemprop="description" ><?=$arResult['PREVIEW_TEXT']?></p>
+                <p class="text toggle-text" itemprop="description"><?= $arResult['PREVIEW_TEXT'] ?></p>
             </div>
         </div>
     </div>
 
-</div>
 </div>
