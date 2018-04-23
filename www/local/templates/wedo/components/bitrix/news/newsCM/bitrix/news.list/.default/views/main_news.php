4c1256d17278ad7ee2beca1c0b74e0e302fcc959
Ahmed Marzouk <AhmedMarzouk266@gmail.com>
2018-04-23 10:06:23 +0300

    сделать теги Ajax

--- www/local/templates/wedo/components/bitrix/news/newsCM/bitrix/news.list/.default/views/main_news.php
+++ www/local/templates/wedo/components/bitrix/news/newsCM/bitrix/news.list/.default/views/main_news.php
@@ -1,5 +1,5 @@
 <div class="col-lg-6 d-none d-lg-block main-news-block">
-    <a href="<?=$item['DETAIL_PAGE_URL']?>" class="wrap-link-main-news">
+    <a href="<?=$item['DETAIL_PAGE_URL']?>" onclick="#" class="wrap-link-main-news">
         <div class="main-news" style="background-image: url('<?=$item['PREVIEW_PICTURE']['SRC']?>')">
             <div class="date-news">
                 <p><?=$item['time']['day']?>:</p>
@@ -14,7 +14,7 @@
 </div>
 <div class="col-lg-3 d-block d-lg-none main-news-block">
     <div class="wrap-news">
-        <a href="<?=$item['DETAIL_PAGE_URL']?>">
+        <a href="<?=$item['DETAIL_PAGE_URL']?>" onclick="#">
             <div class="news" style="background-image:  url('<?=$item['PREVIEW_PICTURE']['SRC']?>')">
                 <div class="date-news date-news_small">
                     <p><?=$item['time']['day']?>:</p>
