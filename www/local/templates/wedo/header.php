63d2d14c51a3948ffc657f9c3c0c89769d1be153
alexkorn22 <kornsasha89@gmail.com>
2018-10-24 11:34:51 +0300

    Исправлены логотипы

--- www/local/templates/wedo/header.php
+++ www/local/templates/wedo/header.php
@@ -35,6 +35,8 @@
     $blackLogo     = new ImageResize(IMAGES_PATH.'/logo_black.png');
     $logoPath      = $logo->getLink(ImageResize::SIZE_LOGO);
     $blackLogoPath = $blackLogo->getLink(ImageResize::SIZE_LOGO);
+    $logoPathWithoutResize = IMAGES_PATH.'/logo.png';
+    $blackLogoPathWithoutResize = IMAGES_PATH.'/logo_black.png';
 
     ?>
 
@@ -59,7 +61,7 @@
                 <div class="col">
                     <div class="wrap-logo">
                         <a href="/">
-                            <img src="<?=$logoPath?>" alt="CMS">
+                            <img src="<?=$logoPathWithoutResize?>" alt="CMS">
                         </a>
                     </div>
                 </div>
@@ -99,7 +101,7 @@
                 <div class="col-4 col-sm-2 col-md-2 col-lg-2">
                     <div class="wrap-logo">
                         <a href="/">
-                            <img src="<?=$blackLogoPath?>" alt="CMS">
+                            <img src="<?=$blackLogoPathWithoutResize?>" alt="CMS">
                         </a>
                     </div>
                 </div>
@@ -137,7 +139,7 @@
                 <div class="wrap-logo">
                     <??>
                     <a href="/">
-                        <img src="<?=$logoPath?>" alt="CMS">
+                        <img src="<?=$logoPathWithoutResize?>" alt="CMS">
                     </a>
                 </div>
                 <div class="burger-menu in-menu">
@@ -227,7 +229,7 @@
                         <div class="col-4 col-sm-2 col-md-2 col-lg-2">
                             <div class="wrap-logo">
                                 <a href="/">
-                                    <img src="<?=$blackLogoPath?>" alt="CMS">
+                                    <img src="<?=$blackLogoPathWithoutResize?>" alt="CMS">
                                 </a>
                             </div>
                         </div>
@@ -266,7 +268,7 @@
                     <div class="row">
                         <div class="wrap-logo">
                             <a href="/">
-                                <img src="<?=$logoPath?>" alt="CMS">
+                                <img src="<?=$logoPathWithoutResize?>" alt="CMS">
                             </a>
                         </div>
                         <div class="burger-menu in-menu">
