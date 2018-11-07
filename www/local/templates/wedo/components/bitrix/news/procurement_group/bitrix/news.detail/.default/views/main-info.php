93b77271d453051452fa55d9a14d2d337cf96ba4
alexkorn22 <kornsasha89@gmail.com>
2018-11-07 16:49:56 +0200

    https://trello.com/c/eHPSMsli CMS-30-663 Закупочные группы. Верстка детального просмотра, основная инфо

--- www/local/templates/wedo/components/bitrix/news/procurement_group/bitrix/news.detail/.default/views/main-info.php
+++ www/local/templates/wedo/components/bitrix/news/procurement_group/bitrix/news.detail/.default/views/main-info.php
@@ -2,10 +2,26 @@
 /** @var ViewTemplate $this */
 /** @var array $arItem */
 
+$props = $arItem['DISPLAY_PROPERTIES'];
 ?>
+<div class="procurement-group-detail__wrap-info">
+    <span class="procurement-group-detail__name-block procurement-group-detail__name-block_bold">Количество оборудования:</span>
+    <span class="procurement-group-detail__name-block"> <?=$props['planned_count']['VALUE']?> / <?=$props['confirmed_count']['VALUE']?></span>
+    <div class="procurement-group-detail__comment text-uppercase">подтверждено / запланировано</div>
+</div>
 
-<h1><?=$arItem['NAME']?></h1>
-<p><span>Количество подтверждено/ запланировано<span> <span><?=$arItem['DISPLAY_PROPERTIES']['planned_count']['VALUE']?>/<?=$arItem['DISPLAY_PROPERTIES']['confirmed_count']?></span></p>
-<p><span>Граничный срок<span> <span><?=$arItem['DISPLAY_PROPERTIES']['period_formation']['VALUE']?></span></p>
+<div class="procurement-group-detail__wrap-info">
+    <div class="procurement-group-detail__name-block procurement-group-detail__name-block_bold">Участники:</div>
+    <div><?=$props['participants']['DISPLAY_VALUE']?></div>
+</div>
 
-<p><span>Текущая ситуация<span> <span><?=$arItem['DISPLAY_PROPERTIES']['current_situation']['DISPLAY_VALUE']?></span></p>
+<div class="procurement-group-detail__wrap-info">
+    <span class="procurement-group-detail__name-block procurement-group-detail__name-block_bold">Граничный срок:</span>
+    <span class="procurement-group-detail__name-block"><?=$props['period_formation']['VALUE']?></span>
+    <div class="procurement-group-detail__comment">до которого выполняется сбор участников</div>
+</div>
+
+<div class="procurement-group-detail__wrap-info">
+    <div class="procurement-group-detail__name-block procurement-group-detail__name-block_bold">Информация для участников:</div>
+    <div><?=$props['current_situation']['DISPLAY_VALUE']?></div>
+</div>
